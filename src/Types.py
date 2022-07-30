2d8f0846ba5ba8e2319ca09318cff8183530dce9
Chicken <christtf8107@hotmail.com>
2022-07-30 10:25:39 +0800

    Started implementation of typing

---
+++ src/Types.py
@@ -0,0 +1,8 @@
+class Type:
+    def __init__(self, typeName):
+        self.typeName = typeName
+        self.typeAdvantage = []
+        self.typeDisadvantage = []
+    
+    def setup():
+        global bugType, dragonType
